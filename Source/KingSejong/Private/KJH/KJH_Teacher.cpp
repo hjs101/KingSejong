58caa695fbec194610b5b21327d17eae81dcef1c
Kang Jihyeon <65221707+Kang-JiHyeon@users.noreply.github.com>
2024-09-02 22:20:23 +0900

    훈장님 기본 상태 만들기

---
+++ Source/KingSejong/Private/KJH/KJH_Teacher.cpp
@@ -0,0 +1,95 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "KJH/KJH_Teacher.h"
+#include "Blueprint/UserWidget.h"
+#include "Components/CapsuleComponent.h"
+#include "Net/UnrealNetwork.h"
+
+AKJH_Teacher::AKJH_Teacher()
+{
+    // Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+    PrimaryActorTick.bCanEverTick = true;
+    bReplicates = true;
+
+    // Collision
+    CapsuleComp = CreateDefaultSubobject<UCapsuleComponent>(TEXT("CapsuleComp"));
+    SetRootComponent(CapsuleComp);
+    CapsuleComp->SetCapsuleHalfHeight(88);
+    CapsuleComp->SetCapsuleRadius(43);
+
+    // SkeletalMesh
+    SkeletalMeshComp = CreateDefaultSubobject<USkeletalMeshComponent>(TEXT("SkeletalMeshComp"));
+    SkeletalMeshComp->SetupAttachment(RootComponent);
+    SkeletalMeshComp->SetRelativeLocationAndRotation(FVector(0, 0, -90), FRotator(0, -90, 0));
+
+
+}
+
+// Called when the game starts or when spawned
+void AKJH_Teacher::BeginPlay()
+{
+    Super::BeginPlay();
+
+}
+
+// Called every frame
+void AKJH_Teacher::Tick(float DeltaTime)
+{
+    Super::Tick(DeltaTime);
+
+}
+
+void AKJH_Teacher::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const
+{
+    Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+
+    DOREPLIFETIME(AKJH_Teacher, TeacherState);
+}
+
+void AKJH_Teacher::OnBeginInteraction(AActor* OtherActor)
+{
+    Super::OnBeginInteraction(OtherActor);
+
+    SetOwner(OtherActor);
+
+    // 질문 UI 열기
+    // 열리는 건 동기화 안되어야 함
+    RecodingWidget = CreateWidget(GetWorld(), RecodingWidgetFactory);
+
+    if ( RecodingWidget )
+    {
+        RecodingWidget->AddToViewport(0);
+    }
+
+    ServerRPCSetTeacherState(ETeacherState::Listen);
+    //SetTeacherState(ETeacherState::Listen);
+}
+
+void AKJH_Teacher::OnEndInteraction()
+{
+    Super::OnEndInteraction();
+
+    // 대기 상태 변경한다.
+    ServerRPCSetTeacherState(ETeacherState::Idle);
+
+    // todo : 위젯 이벤트와 연결해야 함
+
+}
+
+bool AKJH_Teacher::IsInteractable()
+{
+    // 대기 상태일 때만 인터렉션 가능
+    return TeacherState == ETeacherState::Idle;
+}
+
+void AKJH_Teacher::SetTeacherState(ETeacherState NewState)
+{  
+    TeacherState = NewState;
+}
+
+
+void AKJH_Teacher::ServerRPCSetTeacherState_Implementation(ETeacherState NewState)
+{
+    SetTeacherState(NewState);
+}
---
+++ Source/KingSejong/Public/KJH/KJH_Teacher.h
@@ -0,0 +1,58 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "KJH/KJH_InteractiveActor.h"
+#include "KJH_Teacher.generated.h"
+
+UENUM()
+enum class ETeacherState
+{
+	Idle,
+	Listen,
+	Think,
+	Speak
+};
+
+UCLASS()
+class KINGSEJONG_API AKJH_Teacher : public AKJH_InteractiveActor
+{
+	GENERATED_BODY()
+public:
+	// Sets default values for this actor's properties
+	AKJH_Teacher();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+	virtual void GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const override;
+	virtual void OnBeginInteraction(class AActor* OtherActor) override;
+	virtual void OnEndInteraction() override;
+	virtual bool IsInteractable() override;;
+
+public:
+	UPROPERTY(EditDefaultsOnly)
+	class UCapsuleComponent* CapsuleComp;
+	UPROPERTY(EditDefaultsOnly)
+	class USkeletalMeshComponent* SkeletalMeshComp;
+
+	UPROPERTY(VisibleAnywhere, Replicated)
+	ETeacherState TeacherState;
+
+	UPROPERTY(EditDefaultsOnly)
+	TSubclassOf<UUserWidget> RecodingWidgetFactory;
+
+	UPROPERTY()
+	class UUserWidget* RecodingWidget;
+
+public:
+	void SetTeacherState(ETeacherState NewState);
+
+	UFUNCTION(Server, Unreliable)
+	void ServerRPCSetTeacherState(ETeacherState NewState);
+};
