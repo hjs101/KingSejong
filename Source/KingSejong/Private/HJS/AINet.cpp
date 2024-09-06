36053005a9776cd85a0c66980c4a4faa19a9b9ef
h5282001 <gkswlstjdwkd@naver.com>
2024-09-06 19:14:40 +0900

    Feat : 네트워크 파일 생성
    Feat : 네트워크 파일 생성

---
+++ Source/KingSejong/Private/HJS/AINet.cpp
@@ -0,0 +1,34 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "HJS/AINet.h"
+
+// Sets default values for this component's properties
+UAINet::UAINet()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+
+	// ...
+}
+
+
+// Called when the game starts
+void UAINet::BeginPlay()
+{
+	Super::BeginPlay();
+
+	// ...
+	
+}
+
+
+// Called every frame
+void UAINet::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	// ...
+}
+
