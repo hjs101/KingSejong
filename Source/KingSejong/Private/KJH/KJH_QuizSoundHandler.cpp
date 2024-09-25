9440d0a7ad9eae5cde611c4286f9057afbc97ef6
Kang Jihyeon <65221707+Kang-JiHyeon@users.noreply.github.com>
2024-09-25 21:45:22 +0900

    ox 퀴즈 사운드 작업

---
+++ Source/KingSejong/Private/KJH/KJH_QuizSoundHandler.cpp
@@ -0,0 +1,48 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "KJH/KJH_QuizSoundHandler.h"
+#include "KJH/KJH_PlayerController.h"
+#include "Components/AudioComponent.h"
+
+// Sets default values for this component's properties
+UKJH_QuizSoundHandler::UKJH_QuizSoundHandler()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+
+	// ...
+}
+
+
+// Called when the game starts
+void UKJH_QuizSoundHandler::BeginPlay()
+{
+	Super::BeginPlay();
+
+	OwnerPC = Cast<AKJH_PlayerController>(GetOwner());
+
+}
+
+
+// Called every frame
+void UKJH_QuizSoundHandler::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	// ...
+}
+
+void UKJH_QuizSoundHandler::PlayQuizSound(USoundBase* Sound)
+{
+	if (OwnerPC && OwnerPC->AudioComp)
+	{
+		//OwnerPC->ClientPlaySound(Sound);
+		OwnerPC->AudioComp->Stop();
+
+		OwnerPC->AudioComp->Sound = Sound;
+		OwnerPC->AudioComp->Play();
+	}
+}
+
---
+++ Source/KingSejong/Public/KJH/KJH_QuizSoundHandler.h
@@ -0,0 +1,57 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "KJH_QuizSoundHandler.generated.h"
+
+
+UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+class KINGSEJONG_API UKJH_QuizSoundHandler : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:	
+	// Sets default values for this component's properties
+	UKJH_QuizSoundHandler();
+
+protected:
+	// Called when the game starts
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+
+
+public:
+	UPROPERTY(EditDefaultsOnly)
+	class AKJH_PlayerController* OwnerPC;
+
+
+	UPROPERTY(EditDefaultsOnly)
+	class USoundBase* SFX_Correct;
+
+	UPROPERTY(EditDefaultsOnly)
+	class USoundBase* SFX_Fail;
+	
+	UPROPERTY(EditDefaultsOnly)
+	class USoundBase* SFX_BGM;
+
+	UPROPERTY(EditDefaultsOnly)
+	class USoundBase* SFX_Tick;
+
+	UPROPERTY(EditDefaultsOnly)
+	class USoundBase* SFX_Buzzer;
+
+	UPROPERTY(EditDefaultsOnly)
+	class USoundBase* SFX_Notification;
+
+	UPROPERTY(EditDefaultsOnly)
+	class USoundBase* SFX_Bright;
+
+
+public:
+	void PlayQuizSound(class USoundBase* Sound);
+};
